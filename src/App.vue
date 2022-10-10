2907e9128af2886f31124ced5e7c02d28d184f27
rex@vivalnk.com <905578725@qq.com>
2022-10-10 20:06:25 +0800

    主页面背景完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-particles
+      color="#FFF5EE"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#BF3EFF"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.5"
+      :linesDistance="100"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    ></vue-particles>
+    <div style="position: absolute;top: 5%;left: 12%;background-color: white;width: 80%;height: 80%">fadfasf</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 事件的定义
+// 如果使用触摸屏，当滚动时，将会阻止事件发生
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
-</script>
 
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/css/main.css";
+#app{
+  background-color: #E0FFFF;
+  position: relative;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueParticles from 'vue-particles'
+import VueParticlesBg from 'particles-bg-vue'
 
 Vue.config.productionTip = false
+Vue.use(VueParticles)
+Vue.use(VueParticlesBg)
 
 new Vue({
   render: h => h(App)
