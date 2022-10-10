f6a4e8e301880c9b0b954e020940180948b2f011
rex <905578725@qq.com>
2022-10-10 23:39:49 +0800

    页面效果修饰

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <vue-particles
-      color="#FFF5EE"
-      :particleOpacity="0.7"
-      :particlesNumber="80"
-      shapeType="circle"
-      :particleSize="4"
-      linesColor="#BF3EFF"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.5"
-      :linesDistance="100"
-      :moveSpeed="3"
-      :hoverEffect="true"
-      hoverMode="grab"
-      :clickEffect="true"
-      clickMode="push"
-    ></vue-particles>
-    <div style="position: absolute;top: 5%;left: 12%;background-color: white;width: 80%;height: 80%">fadfasf</div>
+    <div style="overflow: auto;position: absolute;width: 100%;height: 100%">
+      <div style="position: absolute;top: 5%;left: 12%;background-color: white;width: 80%;height: 95%">
+        <div style="height: 110%"></div>
+      </div>
+      <vue-particles
+        style="height: 100%"
+        color="#FFF5EE"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#BF3EFF"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.5"
+        :linesDistance="100"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      ></vue-particles>
+    </div>
   </div>
 </template>
 
@@ -35,5 +40,6 @@ export default {
 #app{
   background-color: #E0FFFF;
   position: relative;
+  overflow: auto;
 }
 </style>
